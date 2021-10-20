50dcc052b3eccd2583d8242b7f69632a56d82ba8
Yancey-Ko <57046017+Yancey-Ko@users.noreply.github.com>
2021-10-20 13:52:20 -0400

    Update script.js
    Made the add and remove buttons work properly

--- script.js
+++ script.js
@@ -6,21 +6,38 @@ let colorSelected;
 function addR() {
     //alert("Clicked Add Row");
 	let table = document.getElementById("grid");
-	//if empty table
-	if (numCols == 0 || numRows == 0){
+	//ifemptytablefromstart
+	if (numCols == 0 && numRows == 0){
 		var row = table.insertRow(0);
 		var cells = row.insertCell(0);
-		numRows++;
-		numCols++;
-		cells.innerHTML = numRows;
+		numRows=1;
+		numCols=1;
+		cells.innerHTML = numRows + " " + numCols;
 	}
-	else {
+	//ifrowwasdeletedwithlefovercolumns
+	else if (numCols > 0 && numRows < 1){
+		var row = table.insertRow(0);
+		var cells = row.insertCell(0);
+		numRows=1;
+		numCols=1;
+		cells.innerHTML = numRows + " " + numCols;
+	}
+	//ifcolwasdeletedwithlefoverrows
+	else if (numCols < 1){
+		var row = table.insertRow(0);
+		var cells = row.insertCell(0);
+		numRows=numRows+1;
+		numCols=numCols+1;
+		cells.innerHTML = numRows + " " + numCols;
+	}
+	//ifmorethan1cell
+	else if (numCols > 0 && numRows > 0){
 		var rowlength = document.getElementById("grid").rows[0].cells.length;
 		var newrow = table.insertRow(0);
-		numRows++;
+		numRows=numRows+1;
 		for(var x=0; x<rowlength; x++){
 			var cells = newrow.insertCell(x);
-			cells.innerHTML = numRows;
+			cells.innerHTML = numRows + " " + numCols;
 		}
 	}
 }
@@ -28,31 +45,36 @@ function addR() {
 function addC() {
     //alert("Clicked Add Col");
 	let table = document.getElementById("grid");
-	if (table.rows.length ==0){
+	//ifemptytablefromstart
+	if (table.rows.length == 0){
 		var row = table.insertRow(0);
 		var cells = row.insertCell(0);
-		numRows++;
-		numCols++;
-		cells.innerHTML = numCols;
+		numRows=1;
+		numCols=1;
+		cells.innerHTML = numRows + " " + numCols;
 	}
+	//ifatleast1cell
 	else {
 		var numrows = document.getElementById("grid").rows.length;
 		var rowlength = document.getElementById("grid").rows[0].cells.length;
 		for(var x=0; x<numrows; x++){
 			var cells = table.rows[x].insertCell(rowlength);
 		}
-		cells.innerHTML = 1+numCols++;
+		numCols=numCols+1;
+		cells.innerHTML = numRows + " " + numCols;
 	}
 }
 
 //Remove a row
 function removeR() {
     //alert("Clicked Remove Row")
-	if(numRows > 0 || numCols >0){
+	//whentherearemorethan1row
+	if(numRows > 1){
 		let table = document.getElementById("grid");
 		let row = table.deleteRow(0);
-		numRows--;
+		numRows=numRows-1;
 	}
+	//whenthereis1rowleft
 	else if(numRows == 1){
 		let table = document.getElementById("grid");
 		let row = table.deleteRow(0);
@@ -64,23 +86,43 @@ function removeR() {
 function removeC() {
     //alert("Clicked Remove Col")
 	let table = document.getElementById("grid");
-	if(numRows > 0 || numCols > 0){
-		//let rows = document.getElementById("grid").rows;
+	//ifthereismultiplecolsandrows
+	if(numRows > 1 && numCols > 1){
 		var numrows = document.getElementById("grid").rows.length;
 		var rowlength = document.getElementById("grid").rows[0].cells.length;
 		for(var r=0; r<numrows; r++){
-			table.rows[r].deleteCell(rowlength-1);
+			table.rows[r].deleteCell(-1);
 		}
-		numCols--;
+		numCols=numCols-1;
 	}
-	else if(numCols == 1){
+	//ifthereis1colwith1row
+	else if(numCols == 1 && numRows == 1){
 		var numrows = document.getElementById("grid").rows.length;
 		var rowlength = document.getElementById("grid").rows[0].cells.length;
 		for(var r=0; r<numrows; r++){
-			table.rows[r].deleteCell(rowlength-1);
+			table.deleteRow(r);
+			numRows = numRows-1;
 		}
 		numCols = 0;
-		numRows = 0;
+	}
+	//ifthereis1rowwithmultiplecol
+	else if(numCols > 1 && numRows == 1){
+		var numrows = document.getElementById("grid").rows.length;
+		var rowlength = document.getElementById("grid").rows[0].cells.length;
+		for(var r=0; r<numrows; r++){
+			table.rows[r].deleteCell(0);
+		}
+		numCols = numCols-1;
+	}
+	//ifthereis1colwithmultiplerows
+	else if (numRows > 1 && numCols == 1){
+		var numrows = document.getElementById("grid").rows.length;
+		var rowlength = document.getElementById("grid").rows[0].cells.length;
+		for(var r=0; r<numrows; r++){
+			table.deleteRow(0);
+			numRows = numRows-1;
+		}
+		numCols = 0;
 	}
 }
 //sets global var for selected color
